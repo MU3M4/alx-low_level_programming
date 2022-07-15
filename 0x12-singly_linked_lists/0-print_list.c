aba26919c117ae54985e73f4fc5c2bc400c7ece9
Emmanuel Muema <emmanuelmuemam@gmail.com>
2022-07-15 14:52:43 +0300

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,33 @@
-#include "lists.h"
-/**
- * print_listint - prints all the elements of a linked list
- * @h: head of linked list
- * Return: number of nodes
+/*
+ * File: 0-print_list.c
+ * Auth: Brennan D Baraban
  */
-size_t print_listint(const listint_t *h)
-{
-	unsigned int i = 0;
-	const listint_t *temp_h = h;
 
-	while (temp_h)
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
 	{
-		printf("%d\n", temp_h->n);
-		temp_h = temp_h->next;
-		i++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
+		h = h->next;
 	}
-	return (i);
+
+	return (nodes);
 }
